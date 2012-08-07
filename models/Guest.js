3351aaac48cb14f318904f2e5ccb221d597e1486
Rich Middleditch <richmiddleditch@gmail.com>
2012-08-07 15:48:22 +0100

    JSDoc comments

--- models/Guest.js
+++ models/Guest.js
@@ -9,12 +9,21 @@ var Guest = function () {
 		rsvp: false
 	},
 		methods = {
+		/**
+		 * Uppercase first letter of a string
+		 * @param  {String} string
+		 * @return {String}
+		 */
 		uppercase: function (string) {
 			return string.replace(/^([a-z])/, function ($1) { return $1.toUpperCase(); });
 		}
 	};
 
 	return {
+		/**
+		 * Validates email address and assigns it to the guest
+		 * @param {String} email
+		 */
 		setEmail: function (email) {
 			if (/^[\w!#$%&'*+\-\/=?\^_`{|}~.]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/i.test(email)) {
 				return properties.email = email;
@@ -23,6 +32,10 @@ var Guest = function () {
 			return false;
 		},
 
+		/**
+		 * Ensures @ symbol is present in twitter handle and assigns it to the guest
+		 * @param {String} handle
+		 */
 		setTwitter: function (handle) {
 			if (handle[0] !== '@') {
 				handle = '@' + handle;
@@ -31,10 +44,21 @@ var Guest = function () {
 			return properties.twitter = handle;
 		},
 
+		/**
+		 * Cast state to a boolean
+		 * @param {Boolean} state Will be cast to a boolean
+		 */
 		setRsvp: function (state) {
 			return properties.rsvp = !!state;
 		},
 
+		/**
+		 * Magic getter, will execute get{Property} function if exists. Otherwise returns
+		 * property.
+		 *
+		 * @param  {String} property Property to get
+		 * @return {Mixed}
+		 */
 		get: function (property) {
 			var ucProperty = methods.uppercase(property);
 
@@ -47,6 +71,13 @@ var Guest = function () {
 			return null;
 		},
 
+		/**
+		 * Magic setter, will execute set{Property} if exists.
+		 *
+		 * @param {String} property Property to set
+		 * @param {Mixed} value
+		 * @return {Boolean} true on successful set, false on failure
+		 */
 		set: function (property, value) {
 			var ucProperty = methods.uppercase(property);
 
