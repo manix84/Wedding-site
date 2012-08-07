188d1243801f1266ad9d2d30501c1dc199cac2aa
Rich Middleditch <richmiddleditch@gmail.com>
2012-08-07 10:05:50 +0100

    JSHint-ing

--- models/Guest.js
+++ models/Guest.js
@@ -1,4 +1,4 @@
-var Guest = function() {
+var Guest = function () {
 	var properties = {
 		id: null,
 		firstname: '',
@@ -7,34 +7,34 @@ var Guest = function() {
 	},
 		methods = {
 		uppercase: function (string) {
-			return string.replace(/^([a-z])/, function($1) { return $1.toUpperCase(); });
+			return string.replace(/^([a-z])/, function ($1) { return $1.toUpperCase(); });
 		}
 	};
 
 	return {
-		setEmail: function(email) {
-			if ( /^[\w!#$%&'*+\-\/=?\^_`{|}~.]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/i.test(email) ) {
+		setEmail: function (email) {
+			if (/^[\w!#$%&'*+\-\/=?\^_`{|}~.]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/i.test(email)) {
 				return properties.email = email;
 			}
 
 			return false;
 		},
 
-		get: function(property) {
+		get: function (property) {
 			var ucProperty = methods.uppercase(property);
 			if (this['get' + ucProperty]) {
 				return this['get' + ucProperty]();
-			} else if(property in properties) {
+			} else if (property in properties) {
 				return properties[property];
 			}
 			return null;
 		},
 
-		set: function(property, value) {
+		set: function (property, value) {
 			var ucProperty = methods.uppercase(property);
 			if (this['set' + ucProperty]) {
 				return this['set' + ucProperty](value);
-			} else if(property in properties) {
+			} else if (property in properties) {
 				return properties[property] = value;
 			}
 			return null;
